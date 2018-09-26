ec6437665b2a564df3b74b07857e99a0fe6f97d8
David Millar <david.millar@assurant.com>
2018-09-26 10:13:54 -0500

    Add polyfill for non-Android browsers.

--- stuff.js
+++ stuff.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 6 */
 (function () {
     var userAgent = navigator.userAgent.toString();
     var links = document.getElementsByTagName('a');
@@ -8,8 +9,10 @@
             let venueName = encodeURIComponent(links[i].innerText);
             if (userAgent.match(/iPad|iPhone|iPod/ig)) {
                 links[i].href = 'http://maps.apple.com/?daddr=' + venueName + '&near=' + latLng;
-            } else {
+            } else if (!userAgent.match(/android/ig)) {
                 links[i].href = links[i].href + '?q=' + latLng + '(' + venueName + ')';
+            } else {
+                links[i].href = 'https://www.openstreetmap.org/search?query=' + venueName + '#map=14/' + latLng;
             }
         }
     }
