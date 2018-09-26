074ee42b3c210e64c70cdcfd48470bba2a672735
David Millar <dave@davegoesthedistance.com>
2018-09-26 07:41:52 -0500

    hopefully improve android map experienc

--- stuff.js
+++ stuff.js
@@ -2,12 +2,14 @@
     var userAgent = navigator.userAgent.toString();
     var links = document.getElementsByTagName('a');
     var count = links.length;
-    if (userAgent.match(/iPad|iPhone|iPod/ig)) {
-        for (var i = 0; i < count; i++) {
-            if (links[i].href.indexOf('geo:') > -1) {
-                let latLng = links[i].href.substring(4);
-                let venueName = encodeURIComponent(links[i].innerText);
+    for (var i = 0; i < count; i++) {
+        if (links[i].href.indexOf('geo:') > -1) {
+            let latLng = links[i].href.substring(4);
+            let venueName = encodeURIComponent(links[i].innerText);
+            if (userAgent.match(/iPad|iPhone|iPod/ig)) {
                 links[i].href = 'http://maps.apple.com/?daddr=' + venueName + '&near=' + latLng;
+            } else {
+                links[i].href = links[i].href + '?q=' + latLng + '(' + venueName + ')';
             }
         }
     }
