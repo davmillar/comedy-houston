43cc44516b844b6d924d1cc329a451a8b7f566d9
David Millar <dave@davegoesthedistance.com>
2020-11-23 22:54:14 -0600

    Remove DCH :( and other fixes

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // jshint esversion:6
 var me = self,
-    CACHE_NAME = 'dfwv3.69000003',
+    CACHE_NAME = 'dfwv3.69000004',
     urlsToCache = [
         '/',
         '/?utm_source=web_app_manifest',
